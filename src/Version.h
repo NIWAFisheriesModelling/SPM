3ac3913c6ad3097a50780fd1f20b3d1b429b836e
alistairdunn1 <a.dunn@niwa.co.nz>
2008-11-28 01:43:05 +0000

    Default initial version.h

---
+++ src/Version.h
@@ -0,0 +1,23 @@
+//============================================================================
+// svn_version : A command line utility to extract source control revision
+//               number, date, and time.
+// Author      : S.Rasmussen / A.Dunn
+// Copyright   : Copyright NIWA (c)2008 - www.niwa.co.nz
+//============================================================================
+
+// Warning: This file is an atomatically generated file describing the source
+// control revision number, date, and time using svn_version.
+
+// Call: svn_version --path ../ --output ../src/Version.h --quiet
+// Current working directory: C:\Projects\General\SPM\Debug
+
+#ifndef VERSION_H_
+#define VERSION_H_
+
+#define SOURCE_CONTROL_REVISION 0
+#define SOURCE_CONTROL_DATE "0000-00-00"
+#define SOURCE_CONTROL_YEAR "0000"
+#define SOURCE_CONTROL_MONTH ""
+#define SOURCE_CONTROL_TIME "00:00:00"
+#define SOURCE_CONTROL_VERSION "No version ID available"
+#endif
