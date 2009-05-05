c5b76d5efb7ad6e747382c8eb39a955b627b00e6
alistairdunn1 <a.dunn@niwa.co.nz>
2009-05-05 00:29:03 +0000

    Corrected process error calculation Now expects dTemp to be in positive space

--- src/Likelihoods/Children/CBinomialLikelihood.cpp
+++ src/Likelihoods/Children/CBinomialLikelihood.cpp
@@ -27,8 +27,8 @@ CBinomialLikelihood::CBinomialLikelihood() {
 //**********************************************************************
 double CBinomialLikelihood::adjustErrorValue(double processError, double errorValue) {
   // adjust for N process error
-  if ( (processError >= 0.0) && (errorValue > 0.0) )
-    return (1.0 / (1.0 / errorValue + 1.0 / errorValue));
+  if( (errorValue > 0.0) && (processError > 0.0) )
+    return (1.0/(1.0/errorValue + 1.0/processError));
 
   return errorValue;
 }
@@ -42,18 +42,20 @@ double CBinomialLikelihood::getResult(double expected, double observed, double e
 
   errorValue = adjustErrorValue(processError, errorValue);
 
-  double dTemp = CMath::lnFactorial(errorValue) - CMath::lnFactorial(errorValue * (1 - observed)) - CMath::lnFactorial(
-      errorValue * observed) + errorValue * observed * log(CMath::zeroFun(expected, delta)) + errorValue * (1
-          - observed) * log(CMath::zeroFun(1 - expected, delta));
+  double dTemp = CMath::lnFactorial(errorValue)
+                 - CMath::lnFactorial(errorValue * (1.0 - observed))
+                 - CMath::lnFactorial(errorValue * observed)
+                 + errorValue * observed * log(CMath::zeroFun(expected, delta))
+                 + errorValue * (1.0 - observed) * log(CMath::zeroFun(1.0 - expected, delta));
 
-  return dTemp;
+  return -dTemp;
 }
 
 //**********************************************************************
 // double CBinomialLikelihood::simulateObserved(double expected, double errorValue, double processError, double delta)
 //
 //**********************************************************************
-double CBinomialLikelihood::simulateObserved(double expected, double errorValue, double processError, double delta) {
+double CBinomialLikelihood::simulateObserved(double expected, double errorValue, double ProcessError, double delta) {
   throw string("CBinomialLikelihood.simulateObserved() not yet implemented");
 }
 
