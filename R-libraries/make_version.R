aed82b2ec8fd71b4e08b2c416f2c9d23ccedd475
alistairdunn1 <a.dunn@niwa.co.nz>
2009-05-06 09:17:03 +0000

    Revised version text to reflect date only

--- R-libraries/make_version.R
+++ R-libraries/make_version.R
@@ -5,7 +5,9 @@ promptPackage("spm",filename="spm/man/spm-package.Rd",final=T)
 filename<-"spm/R/spm.binary.version.R"
 cat("\"spm.binary.version\"<-\n",file="spm/R/spm.binary.version.R")
 cat("function() {\n",file=filename,append=T)
-cat(paste("return(\"",system("spm -v",intern=TRUE),"\")\n",sep=""),file=filename,append=T)
+VERSION<-system("spm -v",intern=TRUE)
+VERSION<-substring(VERSION,1,regexpr(" ",VERSION)-1)
+cat(paste("return(\"",VERSION,"\")\n",sep=""),file=filename,append=T)
 cat("}\n",file=filename,append=T)
 #exit
 q()
--- R-libraries/spm/R/spm.binary.version.R
+++ R-libraries/spm/R/spm.binary.version.R
@@ -1,4 +1,4 @@
 "spm.binary.version"<-
 function() {
-return("1.00-2009-05-05 (rev. 3245)")
+return("1.00-2009-05-06")
 }
