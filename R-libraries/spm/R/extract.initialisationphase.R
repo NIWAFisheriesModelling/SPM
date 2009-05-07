2cbdd7f4d22e89bda2d416668e49e3e795a45a1d
alistairdunn1 <a.dunn@niwa.co.nz>
2009-05-07 05:58:20 +0000

    Revised for reformatted reports

--- R-libraries/spm/R/extract.initialisationphase.R
+++ R-libraries/spm/R/extract.initialisationphase.R
@@ -8,8 +8,9 @@ function(file,path="",lines){
   index.start<-(1:length(lines))[substring(lines,1,1)=="["][1]
   index.end<-(1:length(lines))[substring(lines,1,4)=="*end"][1]
   if(index.start >= index.end) stop("Error")
-  col.labs<-spm.string.to.vector.of.words(lines[2+index.start],sep=",")
-  values<-spm.string.to.vector.of.words(lines[(3+index.start):(index.end-1)])
+
+  col.labs<-spm.string.to.vector.of.words(lines[3+index.start],sep=",")
+  values<-spm.string.to.vector.of.words(lines[(4+index.start):(index.end-1)])
   values<-as.data.frame(matrix(values,ncol=length(col.labs),byrow=FALSE))
   names(values)<-col.labs
   values$category<-as.character(values$category)
@@ -19,6 +20,7 @@ function(file,path="",lines){
   res<-list()
   res$label<-substring(lines[index.start],2,nchar(lines[index.start])-1)
   res$report.type<-substring(lines[index.start+1],14)
+  res$phase<-substring(lines[index.start+2],38)
   res$data<-values
   return(res)
 }
--- R-libraries/spm/R/extract.partition.R
+++ R-libraries/spm/R/extract.partition.R
@@ -8,17 +8,19 @@ function(file,path="",lines){
   index.start<-(1:length(lines))[substring(lines,1,1)=="["][1]
   index.end<-(1:length(lines))[substring(lines,1,4)=="*end"][1]
   if(index.start >= index.end) stop("Error")
-  col.labs<-spm.string.to.vector.of.words(lines[2+index.start],sep=",")
-  values<-spm.string.to.vector.of.words(lines[(3+index.start):(index.end-1)])
+  res<-list()
+  res$label<-substring(lines[index.start],2,nchar(lines[index.start])-1)
+  res$report.type<-substring(lines[index.start+1],14)
+  res$year<-substring(lines[index.start+2],17)
+  res$"time_step"<-substring(lines[index.start+3],22)
+  col.labs<-spm.string.to.vector.of.words(lines[4+index.start],sep=",")
+  values<-spm.string.to.vector.of.words(lines[(5+index.start):(index.end-1)])
   values<-as.data.frame(matrix(values,ncol=length(col.labs),byrow=FALSE))
   names(values)<-col.labs
   values$category<-as.character(values$category)
   values$row<-as.numeric(as.character(values$row))
   values$column<-as.numeric(as.character(values$column))
   values[,4:ncol(values)]<-apply(values[,4:ncol(values)],2,as.numeric)
-  res<-list()
-  res$label<-substring(lines[index.start],2,nchar(lines[index.start])-1)
-  res$report.type<-substring(lines[index.start+1],14)
   res$data<-values
   return(res)
 }
