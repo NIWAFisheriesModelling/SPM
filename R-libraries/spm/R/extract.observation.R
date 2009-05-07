c0ada81364b8de7ffb6a89e65c2655d1a4fd7c06
alistairdunn1 <a.dunn@niwa.co.nz>
2009-05-07 05:28:47 +0000

    Revised for revised ObservationReport format

--- R-libraries/spm/R/extract.observation.R
+++ R-libraries/spm/R/extract.observation.R
@@ -11,14 +11,17 @@ function(file,path="",lines){
   res<-list()
   res$label<-substring(lines[index.start],2,nchar(lines[index.start])-1)
   res$report.type<-substring(lines[index.start+1],14)
-  variables<-spm.string.to.vector.of.words(lines[index.start+2],sep=",")
-  data<-spm.string.to.vector.of.words(lines[(index.start+3):(index.end-1)],sep=",")
-  data<<-data
-  data<-matrix(data,ncol=5,byrow=FALSE)
-  data<-data.frame("area"=data[,1], expected=data[,2], observed=data[,3], errorvalue=data[,4], score=data[,5])
+  res$observation.label<-substring(lines[index.start+2],20)
+  variables<-spm.string.to.vector.of.words(lines[index.start+3],sep=",")
+  data<-spm.string.to.vector.of.words(lines[(index.start+4):(index.end-1)],sep=",")
+  data<-matrix(data,ncol=6,byrow=FALSE)
+  data<-as.data.frame(data)
+  names(data)<-variables
+  #data<-data.frame("area"=data[,1], observed=data[,2], expected=data[,3], residual=data[,4], errorvalue=data[,5], score=data[,6])
   data$area<-as.character(data$area)
-  data$expected<-as.numeric(as.character(data$expected))
   data$observed<-as.numeric(as.character(data$observed))
+  data$expected<-as.numeric(as.character(data$expected))
+  data$residuals<-as.numeric(as.character(data$residuals))
   data$errorvalue<-as.numeric(as.character( data$errorvalue))
   data$score<-as.numeric(as.character(data$score))
   res$data<-data
