12fe68dd51c388cca52b44dc6964fcc7c66ce088
alistairdunn1 <a.dunn@niwa.co.nz>
2010-11-30 02:59:56 +0000

    Slight modification for MikTex 2.9

--- R-libraries/spm/R/spm.binary.version.R
+++ R-libraries/spm/R/spm.binary.version.R
@@ -1,4 +1,4 @@
 "spm.binary.version"<-
 function() {
-return("1.00-2010-01-11")
+return("1.00-2010-11-30")
 }
--- doc/manual/Build.bat
+++ doc/manual/Build.bat
@@ -1,15 +1,15 @@
 svn_version --path ..\ --format tex --recursive --quiet --suffix Doc
 "../../spm" -v > SPMversion.tex
 python QuickReference.py
-"C:\Program Files\MiKTeX 2.7\miktex\bin\pdflatex.exe" SPM
-"C:\Program Files\MiKTeX 2.7\miktex\bin\bibtex.exe" SPM
-"C:\Program Files\MiKTeX 2.7\miktex\bin\makeindex.exe" SPM
+"C:\Program Files\MiKTeX 2.9\miktex\bin\pdflatex.exe" SPM
+"C:\Program Files\MiKTeX 2.9\miktex\bin\bibtex.exe" SPM
+"C:\Program Files\MiKTeX 2.9\miktex\bin\makeindex.exe" SPM
 python QuickReference.py
-"C:\Program Files\MiKTeX 2.7\miktex\bin\pdflatex.exe" SPM
-"C:\Program Files\MiKTeX 2.7\miktex\bin\bibtex.exe" SPM
-"C:\Program Files\MiKTeX 2.7\miktex\bin\makeindex.exe" SPM
+"C:\Program Files\MiKTeX 2.9\miktex\bin\pdflatex.exe" SPM
+"C:\Program Files\MiKTeX 2.9\miktex\bin\bibtex.exe" SPM
+"C:\Program Files\MiKTeX 2.9\miktex\bin\makeindex.exe" SPM
 python QuickReference.py
-"C:\Program Files\MiKTeX 2.7\miktex\bin\pdflatex.exe" SPM
-"C:\Program Files\MiKTeX 2.7\miktex\bin\bibtex.exe" SPM
-"C:\Program Files\MiKTeX 2.7\miktex\bin\makeindex.exe" SPM
+"C:\Program Files\MiKTeX 2.9\miktex\bin\pdflatex.exe" SPM
+"C:\Program Files\MiKTeX 2.9\miktex\bin\bibtex.exe" SPM
+"C:\Program Files\MiKTeX 2.9\miktex\bin\makeindex.exe" SPM
 
