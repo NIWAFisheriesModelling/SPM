2852783eb4191b8345ea4a3b367937d9c9d7ac22
alistairdunn1 <a.dunn@niwa.co.nz>
2012-05-09 09:55:51 +0000

    CHG: Reformatted line breaks

--- R-libraries/spm/R/extract.R
+++ R-libraries/spm/R/extract.R
@@ -22,8 +22,7 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
     }
 
     index <- (1:length(line))[substring(line, 1, 1) == "["]
-    report.label <- substring(line[index], 2, nchar(line[index]) -
-        1)
+    report.label <- substring(line[index], 2, nchar(line[index]) - 1)
     report.type <- substring(line[index + 1], 14)
     if (length(report.label) != length(report.type))
         stop("Error")
@@ -36,10 +35,8 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
             if (!("initialisation" %in% names(res))) {
                 res$initialisation <- list()
             }
-            counter$initialisation <- counter$initialisation +
-                1
-            temp <- extract.initialisationphase(lines = line[index[i]:(index[i +
-                1] - 1)])
+            counter$initialisation <- counter$initialisation + 1
+            temp <- extract.initialisationphase(lines = line[index[i]:(index[i + 1] - 1)])
             res$initialisation[[counter$initialisation]] <- temp
             names(res$initialisation)[counter$initialisation] <- report.label[i]
         }
@@ -48,8 +45,7 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
                 res$partition <- list()
             }
             counter$partition <- counter$partition + 1
-            temp <- extract.partition(lines = line[index[i]:(index[i +
-                1] - 1)])
+            temp <- extract.partition(lines = line[index[i]:(index[i + 1] - 1)])
             res$partition[[counter$partition]] <- temp
             names(res$partition)[counter$partition] <- report.label[i]
         }
@@ -57,10 +53,8 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
             if (!("layer_derived_world_view" %in% names(res))) {
                 res$layer_derived_world_view <- list()
             }
-            counter$layer_derived_world_view <- counter$layer_derived_world_view +
-                1
-            temp <- extract.layerderivedworldview(lines = line[index[i]:(index[i +
-                1] - 1)])
+            counter$layer_derived_world_view <- counter$layer_derived_world_view + 1
+            temp <- extract.layerderivedworldview(lines = line[index[i]:(index[i + 1] - 1)])
             res$layer_derived_world_view[[counter$layer_derived_world_view]] <- temp
             names(res$layer_derived_world_view)[counter$layer_derived_world_view] <- report.label[i]
         }
@@ -69,8 +63,7 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
                 res$process <- list()
             }
             counter$process <- counter$process + 1
-            temp <- extract.process(lines = line[index[i]:(index[i +
-                1] - 1)])
+            temp <- extract.process(lines = line[index[i]:(index[i + 1] - 1)])
             res$process[[counter$process]] <- temp
             names(res$process)[counter$process] <- report.label[i]
         }
@@ -78,10 +71,8 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
             if (!("derived_quantity" %in% names(res))) {
                 res$derived_quantity <- list()
             }
-            counter$derived_quantity <- counter$derived_quantity +
-                1
-            temp <- extract.derivedquantity(lines = line[index[i]:(index[i +
-                1] - 1)])
+            counter$derived_quantity <- counter$derived_quantity + 1
+            temp <- extract.derivedquantity(lines = line[index[i]:(index[i + 1] - 1)])
             res$derived_quantity[[counter$derived_quantity]] <- temp
             names(res$derived_quantity)[counter$derived_quantity] <- report.label[i]
         }
@@ -89,10 +80,8 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
             if (!("estimate_summary" %in% names(res))) {
                 res$estimate_summary <- list()
             }
-            counter$estimate_summary <- counter$estimate_summary +
-                1
-            temp <- extract.estimatesummary(lines = line[index[i]:(index[i +
-                1] - 1)])
+            counter$estimate_summary <- counter$estimate_summary + 1
+            temp <- extract.estimatesummary(lines = line[index[i]:(index[i + 1] - 1)])
             res$estimate_summary[[counter$estimate_summary]] <- temp
             names(res$estimate_summary)[counter$estimate_summary] <- report.label[i]
         }
@@ -100,10 +89,8 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
             if (!("objective_function" %in% names(res))) {
                 res$objective_function <- list()
             }
-            counter$objective_function <- counter$objective_function +
-                1
-            temp <- extract.objectivefunction(lines = line[index[i]:(index[i +
-                1] - 1)])
+            counter$objective_function <- counter$objective_function + 1
+            temp <- extract.objectivefunction(lines = line[index[i]:(index[i + 1] - 1)])
             res$objective_function[[counter$objective_function]] <- temp
             names(res$objective_function)[counter$objective_function] <- report.label[i]
         }
@@ -112,8 +99,7 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
                 res$observation <- list()
             }
             counter$observation <- counter$observation + 1
-            temp <- extract.observation(lines = line[index[i]:(index[i +
-                1] - 1)])
+            temp <- extract.observation(lines = line[index[i]:(index[i + 1] - 1)])
             res$observation[[counter$observation]] <- temp
             names(res$observation)[counter$observation] <- report.label[i]
         }
@@ -122,8 +108,7 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
                 res$spatial_map <- list()
             }
             counter$spatial_map <- counter$spatial_map + 1
-            temp <- extract.spatial.map(lines = line[index[i]:(index[i +
-                1] - 1)])
+            temp <- extract.spatial.map(lines = line[index[i]:(index[i + 1] - 1)])
             res$spatial_map[[counter$spatial_map]] <- temp
             names(res$spatial_map)[counter$spatial_map] <- report.label[i]
         }
@@ -132,8 +117,7 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
                 res$layer <- list()
             }
             counter$layer <- counter$layer + 1
-            temp <- extract.layer(lines = line[index[i]:(index[i +
-                1] - 1)])
+            temp <- extract.layer(lines = line[index[i]:(index[i +  1] - 1)])
             res$layer[[counter$layer]] <- temp
             names(res$layer)[counter$layer] <- report.label[i]
         }
@@ -141,10 +125,8 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
             if (!("layer_derived_view" %in% names(res))) {
                 res$layer_derived_view <- list()
             }
-            counter$layer_derived_view <- counter$layer_derived_view +
-                1
-            temp <- extract.layerderivedview(lines = line[index[i]:(index[i +
-                1] - 1)])
+            counter$layer_derived_view <- counter$layer_derived_view + 1
+            temp <- extract.layerderivedview(lines = line[index[i]:(index[i + 1] - 1)])
             res$layer_derived_view[[counter$layer_derived_view]] <- temp
             names(res$layer_derived_view)[counter$layer_derived_view] <- report.label[i]
         }
@@ -153,8 +135,7 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
                 res$selectivity <- list()
             }
             counter$selectivity <- counter$selectivity + 1
-            temp <- extract.selectivity(lines = line[index[i]:(index[i +
-                1] - 1)])
+            temp <- extract.selectivity(lines = line[index[i]:(index[i + 1] - 1)])
             res$selectivity[[counter$selectivity]] <- temp
             names(res$selectivity)[counter$selectivity] <- report.label[i]
         }
@@ -162,10 +143,8 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
             if (!("random_number_seed" %in% names(res))) {
                 res$random_number_seed <- list()
             }
-            counter$random_number_seed <- counter$random_number_seed +
-                1
-            temp <- extract.randomnumberseed(lines = line[index[i]:(index[i +
-                1] - 1)])
+            counter$random_number_seed <- counter$random_number_seed + 1
+            temp <- extract.randomnumberseed(lines = line[index[i]:(index[i + 1] - 1)])
             res$random_number_seed[[counter$random_number_seed]] <- temp
             names(res$random_number_seed)[counter$random_number_seed] <- report.label[i]
         }
@@ -173,10 +152,8 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
             if (!("weight_at_size" %in% names(res))) {
                 res$weight_at_size <- list()
             }
-            counter$weight_at_size <- counter$weight_at_size +
-                1
-            temp <- extract.weightatsize(lines = line[index[i]:(index[i +
-                1] - 1)])
+            counter$weight_at_size <- counter$weight_at_size + 1
+            temp <- extract.weightatsize(lines = line[index[i]:(index[i + 1] - 1)])
             res$weight_at_size[[counter$weight_at_size]] <- temp
             names(res$weight_at_size)[counter$weight_at_size] <- report.label[i]
         }
@@ -184,10 +161,8 @@ extract <- function (file, path = "", ignore.unknown=TRUE)
             if (!("covariance" %in% names(res))) {
                 res$covariance <- list()
             }
-            counter$covariance <- counter$covariance +
-                1
-            temp <- extract.covariance(lines = line[index[i]:(index[i +
-                1] - 1)])
+            counter$covariance <- counter$covariance + 1
+            temp <- extract.covariance(lines = line[index[i]:(index[i + 1] - 1)])
             res$covariance[[counter$covariance]] <- temp
             names(res$covariance)[counter$covariance] <- report.label[i]
         }
