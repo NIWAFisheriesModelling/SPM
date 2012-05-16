65081e03c39dd9d2e878f42563cc5676000a9850
alistairdunn1 <a.dunn@niwa.co.nz>
2012-05-16 23:01:11 +0000

    CHG: Revised input and output for all test cases based on latest version of spm

---
+++ TestCases/1x1_Examples/run.bat
@@ -0,0 +1,21 @@
+cd AgeingProcess
+spm -r -q > Expected.txt
+cd ..
+cd CategoryTransitionProcess
+spm -r -q > Expected.txt
+cd ..
+cd CategoryTransitionRateProcess
+spm -r -q > Expected.txt
+cd ..
+cd ConstantRecruitmentProcess
+spm -r -q > Expected.txt
+cd ..
+cd EventMortalityProcess
+spm -r -q > Expected.txt
+cd ..
+cd NaturalMortalityProcess
+spm -r -q > Expected.txt
+cd ..
+cd ProportionsAtAgeObservation
+spm -r -q > Expected.txt
+cd ..
