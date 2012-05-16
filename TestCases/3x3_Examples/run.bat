12584fab6b682824047bf66c4636147334e6c2f4
alistairdunn1 <a.dunn@niwa.co.nz>
2012-05-16 00:38:20 +0000

    CHG: Updated for latest spm version ADD: AdjacentCell movement TestCase

---
+++ TestCases/3x3_Examples/run.bat
@@ -0,0 +1,27 @@
+cd AdjacentCellMovementProcess
+spm -r -q > Expected.txt
+cd ..
+cd CategoricalPreferenceMovementProcess
+spm -r -q > Expected.txt
+cd ..
+cd ConstantPreferenceMovementProcess
+spm -r -q > Expected.txt
+cd ..
+cd DoubleNormalPreferenceMovementProcess
+spm -r -q > Expected.txt
+cd ..
+cd ExponentialPreferenceMovementProcess
+spm -r -q > Expected.txt
+cd ..
+cd InverseLogisticPreferenceMovementProcess
+spm -r -q > Expected.txt
+cd ..
+cd LogisticPreferenceMovementProcess
+spm -r -q > Expected.txt
+cd ..
+cd MonotonicCategoricalPreferenceMovementProcess
+spm -r -q > Expected.txt
+cd ..
+cd NormalPreferenceMovementProcess
+spm -r -q > Expected.txt
+cd ..
