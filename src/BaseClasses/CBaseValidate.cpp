46243c39cc41caeaed0f33a02a65f41ff09403dc
alistairdunn1 <a.dunn@niwa.co.nz>
2012-06-18 23:59:59 +0000

    ADD: Added hyphen (-) as an allowed character in a label

--- src/BaseClasses/CBaseValidate.cpp
+++ src/BaseClasses/CBaseValidate.cpp
@@ -39,7 +39,7 @@ void CBaseValidate::validate() {
 
     // Check label is alphanumeric (incl. '_')
     for (size_t i=0; i < sLabel.length(); ++i) {
-      if ( !(isalnum(sLabel.at(i)) || sLabel.at(i)== boost::lexical_cast<char>("_")) )
+      if ( !(isalnum(sLabel.at(i)) || sLabel.at(i)== boost::lexical_cast<char>("_") || sLabel.at(i)== boost::lexical_cast<char>("-") ) )
         CError::errorInvalidCharacter(boost::lexical_cast<string>(sLabel.at(i)),sLabel);
     }
 
