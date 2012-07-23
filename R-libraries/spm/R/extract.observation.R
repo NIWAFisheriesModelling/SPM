c50e36915550b162f85564de2aa8d6c4bd4f9499
alistairdunn1 <a.dunn@niwa.co.nz>
2012-07-23 21:45:36 +0000

    FIX: Revised to deal with observations with no data (this is an error in apm, but it may happen occasionally)

--- R-libraries/spm/R/extract.observation.R
+++ R-libraries/spm/R/extract.observation.R
@@ -9,17 +9,22 @@ function(lines){
   res$report.type<-substring(lines[index.start+1],14)
   res$observation.label<-substring(lines[index.start+2],20)
   variables<-spm.string.to.vector.of.words(lines[index.start+3],sep=",")
-  data<-spm.string.to.vector.of.words(lines[(index.start+4):(index.end-1)],sep=",")
-  data<-matrix(data,ncol=6,byrow=TRUE)
-  data<-as.data.frame(data)
-  names(data)<-variables
-  #data<-data.frame("area"=data[,1], observed=data[,2], expected=data[,3], residual=data[,4], errorvalue=data[,5], score=data[,6])
-  data$area<-as.character(data$area)
-  data$observed<-as.numeric(as.character(data$observed))
-  data$expected<-as.numeric(as.character(data$expected))
-  data$residual<-as.numeric(as.character(data$residual))
-  data$errorvalue<-as.numeric(as.character( data$errorvalue))
-  data$score<-as.numeric(as.character(data$score))
-  res$data<-data
+  if((index.end-index.start)>4) {
+    data<-spm.string.to.vector.of.words(lines[(index.start+4):(index.end-1)],sep=",")
+    data<-matrix(data,ncol=6,byrow=TRUE)
+    data<-as.data.frame(data)
+    names(data)<-variables
+    data<-data.frame("area"=data[,1], observed=data[,2], expected=data[,3], residual=data[,4], errorvalue=data[,5], score=data[,6])
+    data$area<-as.character(data$area)
+    data$observed<-as.numeric(as.character(data$observed))
+    data$expected<-as.numeric(as.character(data$expected))
+    data$residual<-as.numeric(as.character(data$residual))
+    data$errorvalue<-as.numeric(as.character( data$errorvalue))
+    data$score<-as.numeric(as.character(data$score))
+    res$data<-data
+  } else {
+    res$data<-"No data"
+  }
   return(res)
 }
+
