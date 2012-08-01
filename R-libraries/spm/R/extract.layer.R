0b2426643008587e06d3356c18e111432a20625f
alistairdunn1 <a.dunn@niwa.co.nz>
2012-08-01 10:49:35 +0000

    FIX: silly R error

--- R-libraries/spm/R/extract.layer.R
+++ R-libraries/spm/R/extract.layer.R
@@ -5,11 +5,11 @@ function(lines){
   index.end<-(1:length(lines))[substring(lines,1,4)=="*end"][1]
   if(index.start >= index.end) stop("Error")
   values<-spm.string.to.vector.of.words(lines[(4+index.start):(index.end-1)],sep=" ")
+  res <- list()
   res$year<-substring(lines[index.start+2],7)
   res$"time_step"<-substring(lines[index.start+3],12)
   nrows<-length((4+index.start):(index.end-1))
   values<-matrix(values,nrow=nrows,byrow=TRUE)
-  res<-list()
   res$label<-substring(lines[index.start],2,nchar(lines[index.start])-1)
   res$report.type<-substring(lines[index.start+1],14)
   res$data<-values
