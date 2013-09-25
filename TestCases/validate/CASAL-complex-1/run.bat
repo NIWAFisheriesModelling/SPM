948356e9e4375ba0beb27dcbba805d69a27c9e8d
alistairdunn1 <a.dunn@niwa.co.nz>
2013-09-25 17:27:40 +0000

    FIX: fixed comment

--- TestCases/validate/CASAL-complex-1/run.bat
+++ TestCases/validate/CASAL-complex-1/run.bat
@@ -1,4 +1,4 @@
-# Basic numbers only model, no catch-at observations
+rem Basic numbers only model, no catch-at observations
 casal -r -q > CASAL-run.log
 spm -r -q > SPM-run.log
 casal -e -q > CASAL-estimate.log
