15f1c9f52398e26728711cb8f3d73ec67833130f
alistairdunn1 <a.dunn@niwa.co.nz>
2014-02-22 00:50:24 +0000

    ADD: LogNormalWithQLikelihood unit tests CHG: Added additional tests for LogNormalLikelihood

--- src/Likelihoods/Children/CLogNormalWithQLikelihood.Test.cpp
+++ src/Likelihoods/Children/CLogNormalWithQLikelihood.Test.cpp
@@ -25,11 +25,11 @@
 //
 //
 //**********************************************************************
-BOOST_AUTO_TEST_CASE(LogNormalLikelihood ) {
+BOOST_AUTO_TEST_CASE(LogNormalWithQLikelihood ) {
   CRandomNumberGenerator::Instance()->Reset(31373u);
 
   // Get Likelihood
-  CLikelihood *pLikelihood = CLikelihoodFactory::buildLikelihood(PARAM_PROPORTIONS_AT_AGE, PARAM_LOGNORMAL);
+  CLikelihood *pLikelihood = CLikelihoodFactory::buildLikelihood(PARAM_BIOMASS, PARAM_LOGNORMAL);
 
   // Generate data
   vector<string> keys;
@@ -71,64 +71,17 @@ BOOST_AUTO_TEST_CASE(LogNormalLikelihood ) {
   pLikelihood->simulateObserved(keys, observed, expected, errorValue, processError, delta);
 
   // Check results
-  BOOST_CHECK_CLOSE(observed[0], 0.25001425607411909, 1e-9);
-  BOOST_CHECK_CLOSE(observed[1], 0.2500028540353656,  1e-9);
-  BOOST_CHECK_CLOSE(observed[2], 0.24998304181077338, 1e-9);
-  BOOST_CHECK_CLOSE(observed[3], 0.24999984807974196, 1e-9);
-  BOOST_CHECK_CLOSE(observed[4], 0.32993903406685038, 1e-9);
-  BOOST_CHECK_CLOSE(observed[5], 0.14708804198606557, 1e-9);
-  BOOST_CHECK_CLOSE(observed[6], 0.33929924175930265, 1e-9);
-  BOOST_CHECK_CLOSE(observed[7], 0.18367368218778146, 1e-9);
+  BOOST_CHECK_CLOSE(observed[0], 0.250014256074119, 1e-9);
+  BOOST_CHECK_CLOSE(observed[1], 0.250002854035365, 1e-9);
+  BOOST_CHECK_CLOSE(observed[2], 0.249983041810773, 1e-9);
+  BOOST_CHECK_CLOSE(observed[3], 0.250015336159831, 1e-9);
+  BOOST_CHECK_CLOSE(observed[4], 0.467105011795629, 1e-9);
+  BOOST_CHECK_CLOSE(observed[5], 0.208237142298769, 1e-9);
+  BOOST_CHECK_CLOSE(observed[6], 0.480356550635094, 1e-9);
+  BOOST_CHECK_CLOSE(observed[7], 0.260032577617012, 1e-9);
 
   // clear memory
   delete pLikelihood;
-}
-
-//**********************************************************************
-//
-//
-//**********************************************************************
-BOOST_FIXTURE_TEST_CASE( LogNormalLikelihood_1x1, C1x1_Fixture ) {
-
-  // Add What we need to configuration
-  // Then run our model
-  addToConfiguration(ageing);
-  addToConfiguration(constant_recruitment);
-  addToConfiguration(age_size_vonB);
-  addToConfiguration(size_weight_none);
-
-  addToTimeStep("ageing constant_recruitment");
-  loadAndRunEnvironment();
-
-  // Check our Results
-
-  // Get Likelihood
-  CLikelihood *pLikelihood = CLikelihoodFactory::buildLikelihood(PARAM_PROPORTIONS_AT_AGE, PARAM_LOGNORMAL);
-
-  // Check results
-//  BOOST_CHECK_EQUAL(pLikelihood->simulateObserved(1.0, 0.0001, 0.001, 1e-11), 0.99845193414953692);
-//  BOOST_CHECK_EQUAL(pLikelihood->simulateObserved(1.0, 0.0001, 0.001, 1e-11), 1.0010263962677095);
-//  BOOST_CHECK_EQUAL(pLikelihood->simulateObserved(1.0, 0.0001, 0.001, 1e-11), 0.99947282571698015);
-//  BOOST_CHECK_EQUAL(pLikelihood->simulateObserved(1.0, 0.0001, 0.001, 1e-11), 0.99945174404156234);
-//  BOOST_CHECK_EQUAL(pLikelihood->simulateObserved(1.0, 0.0001, 0.001, 1e-11), 0.9992072565815735);
-
-  // clear memory
-  delete pLikelihood;
-
-  // Check Population
-  CWorldSquare *pSquare = getSquare();
-
-  BOOST_CHECK_EQUAL(pSquare->getAbundance(), 1500.0);
-
-  // Immature only should've aged
-  for (int i = 0; i < 9; ++i)
-    BOOST_CHECK_EQUAL(pSquare->getValue(0, i), 100.0);
-  BOOST_CHECK_EQUAL(pSquare->getValue(0, 9), 600.0);
-
-  // No population should've been recruited here
-  for (int i = 1; i < 3; ++i)
-    for (int j = 0; j < 10; ++j)
-      BOOST_CHECK_EQUAL(pSquare->getValue(i, j), 0.0);
 }
 
 #endif /* TEST */
