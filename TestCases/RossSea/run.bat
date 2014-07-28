5a99d5e0eedb6a0bdcb231e774e20d31303ba3b5
Alistair Dunn <a.dunn@niwa.co.nz>
2014-07-28 15:54:30 +1200

    ADD: New testcases that may produce errors

---
+++ TestCases/RossSea/run.bat
@@ -0,0 +1,17 @@
+cd coarse\R1.2\
+start run.bat
+cd ..
+cd ..
+cd fine\r1.2\
+start run.bat
+cd ..
+cd ..
+cd fine-semirestricted\r50.04c.threaded\
+start run.bat
+cd ..
+cd ..
+cd Medium\r4.0\
+start run.bat
+exit
+
+
