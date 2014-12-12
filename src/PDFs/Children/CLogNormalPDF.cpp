50efcc04469e5f2a0d3e26b63ec137d7bf91c45e
Alistair Dunn <a.dunn@niwa.co.nz>
2014-12-12 13:54:59 +1300

    FIX: New formula for Frank and Gumbel copula FIX: New formula for lognormal pdf

--- src/PDFs/Children/CLogNormalPDF.cpp
+++ src/PDFs/Children/CLogNormalPDF.cpp
@@ -89,8 +89,25 @@ double CLogNormalPDF::getCDFResult(double value) {
   try {
 #endif
 
-    double Z = (log(value) - dMu)/dSigma;
-    dRet = exp(-(Z * Z))/sqrt(PI);
+  double x = (log(value)-dMu)/dSigma;
+  // constants for the error function
+  double a1 =  0.254829592;
+  double a2 = -0.284496736;
+  double a3 =  1.421413741;
+  double a4 = -1.453152027;
+  double a5 =  1.061405429;
+  double p  =  0.3275911;
+
+  double sign = 1.0;
+  if(x < 0) {
+    sign = -1.0;
+  }
+
+  x = fabs(x)/sqrt(2.0);
+  double t = 1.0/(1.0 + p*x);
+  double y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*exp(-x*x);
+
+  dRet = (0.5*(1.0 + sign*y));
 
 #ifndef OPTIMIZE
   } catch (string &Ex) {
--- src/PreferenceFunctions/Children/CGumbelPreferenceFunction.cpp
+++ src/PreferenceFunctions/Children/CGumbelPreferenceFunction.cpp
@@ -126,10 +126,11 @@ double CGumbelPreferenceFunction::getResult(int RIndex, int CIndex, int TRIndex,
     double dCDF1 = vPDFs[0]->getCDFResult(x1);
     double dCDF2 = vPDFs[1]->getCDFResult(x2);
 
-    dRet = exp(- pow(-log(dCDF1),dRho));// blah blah .. * dPDF1 * dPDF2;
-
-    //dRet = exp(- ((-log(dCDF1))^dRho + (-log(dCDF2))^dRho)^(1/dRho) * (((-log(dCDF1))^(dRho-1))/dCDF1) * (((-log(dCDF2))^(dRho-1))/dCDF2) *
-    //       (((-log(dCDF1))^dRho + (-log(dCDF2))^dRho)^((2/dRho)-2) + ((dRho-1) * ((-log(dCDF1))^dRho + (-log(dCDF2))^Theta)^((1/(dRho)-2)))))* dPDF1 * dPDF2;
+    dRet = exp(- pow(pow(-log(dCDF1),dRho) + pow(-log(dCDF2),dRho),(1.0/dRho))) *
+           (pow(-log(dCDF1),(dRho-1.0))/dCDF1) * (pow(-log(dCDF2),(dRho-1.0))/dCDF2) *
+           (pow(pow(-log(dCDF1),dRho) + pow(-log(dCDF2),dRho),((2.0/dRho)-2.0)) + ((dRho-1.0) *
+           pow(pow(-log(dCDF1),dRho) + pow(-log(dCDF2),dRho),((1.0/(dRho)-2.0))))) *
+           dPDF1 * dPDF2;
 
 #ifndef OPTIMIZE
   } catch (string &Ex) {
